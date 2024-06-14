7f37206efd38734bcda567eb1938ecbb2b1c1d08
LordMcSniff <johann.schoeps1+github@gmail.com>
2024-06-14 20:13:11 +0200

    Highest Respone Ratio Next done

--- src/HRRN.c
+++ src/HRRN.c
@@ -1,27 +1,39 @@
 #include "../lib/HRRN.h"
+#include "queue_extended.h"
 
 static queue_object *HRRN_queue;
+unsigned int tick = 0;
 //You can add more global variables and structs here
 
 process *HRRN_tick(process *running_process)
 {
-	// TODO
-	return NULL;
+	// print_queue(HRRN_queue);
+	if (running_process == NULL || running_process->time_left == 0)
+		running_process = queue_pop_highest_response_ratio(HRRN_queue, tick++);
+
+	if (running_process != NULL)
+		running_process->time_left--;
+
+	return running_process;
 }
 
 int HRRN_startup()
 {
-	// TODO
-	return 1;
+	HRRN_queue = new_queue();
+	if (HRRN_queue == NULL)
+		return 1;
+	return 0;
 }
 
 process *HRRN_new_arrival(process *arriving_process, process *running_process)
 {
-	// TODO
-	return NULL;
+	if (arriving_process != NULL)
+		queue_add(arriving_process, HRRN_queue);
+
+	return running_process;
 }
 
 void HRRN_finish()
 {
-	// TODO
+	free_queue(HRRN_queue);
 }
