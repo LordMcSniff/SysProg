e134d7c449be6816ca336b1693a9488d4ae580dd
LordMcSniff <johann.schoeps1+github@gmail.com>
2024-06-14 20:12:44 +0200

    Priority Preamptive done

--- src/PRIOP.c
+++ src/PRIOP.c
@@ -1,4 +1,5 @@
 #include "../lib/PRIOP.h"
+#include "queue_extended.h"
 #include <stdio.h>
 
 static queue_object *PRIOP_queue;
@@ -6,23 +7,42 @@ static queue_object *PRIOP_queue;
 
 process *PRIOP_tick(process *running_process)
 {
-	// TODO
-	return NULL;
+	// print_queue(PRIOP_queue);
+	if (running_process == NULL || running_process->time_left == 0)
+		running_process = queue_pop(PRIOP_queue);
+
+	if (running_process != NULL)
+		running_process->time_left--;
+
+	return running_process;
 }
 
 int PRIOP_startup()
 {
-	// TODO
-	return 1;
+	PRIOP_queue = new_queue();
+	if (PRIOP_queue == NULL)
+		return 1;
+	return 0;
 }
 
 process *PRIOP_new_arrival(process *arriving_process, process *running_process)
 {
-	// TODO
-	return NULL;
+	if (arriving_process != NULL)
+	{
+		if (running_process == NULL || arriving_process->priority > running_process->priority)
+		{
+			process *swap = running_process;
+			running_process = arriving_process;
+			arriving_process = swap;
+		}
+
+		if (arriving_process != NULL)
+			queue_insert_priority(arriving_process, PRIOP_queue);
+	}
+	return running_process;
 }
 
 void PRIOP_finish()
 {
-	// TODO
+	free_queue(PRIOP_queue);
 }
